27cc7173bd4bf685ffa6cc4579f8800bcf8f6ec4
Abdurahman Usdi <157474429+AbduUSDI@users.noreply.github.com>
2024-08-27 22:10:51 +0200

    ok

--- app/config/Database.php
+++ app/config/Database.php
@@ -4,14 +4,15 @@ namespace App\Config;
 use PDO;
 use PDOException;
 use Exception;
+
 /**
  * Classe pour établir une connexion à la base de données
  */
 class Database {
     private $hote = 'cky0ko.stackhero-network.com';
     private $nom_base_de_donnees = 'e_learning';
-    private $identifiant = 'Abdurahman';
-    private $mot_de_passe = 'Abdufufu2525+';
+    private $identifiant = 'root';
+    private $mot_de_passe = 'a1QDfFRVtBpNgZvJB8cy0dtUlllKXyOL';
     private $port = '5040';
     private $connexion;
 
@@ -19,10 +20,17 @@ class Database {
         $this->connexion = null;
 
         try {
+            // Ajouter les options SSL
+            $options = [
+                PDO::MYSQL_ATTR_SSL_CA => "C:\\Users\\Abdu's PC\\Downloads\\isrgrootx1 (1).pem",
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ];
+
             $this->connexion = new PDO(
                 "mysql:host=" . $this->hote . ";port=" . $this->port . ";dbname=" . $this->nom_base_de_donnees,
                 $this->identifiant,
-                $this->mot_de_passe
+                $this->mot_de_passe,
+                $options // Ajout des options SSL
             );
 
             $this->connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
